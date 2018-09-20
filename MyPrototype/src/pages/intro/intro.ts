7a26dab7e35aa7a4c5123637d7b548df188a66a1
Jessica Thomsen <>
2018-09-20 12:05:52 +0200

    Intro Slider Struture

--- MyPrototype/src/pages/intro/intro.ts
+++ MyPrototype/src/pages/intro/intro.ts
@@ -18,6 +18,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class IntroPage {
 	 public homePage = HomePage;
 
+slides = [
+    {
+      title: "Welcome to Bysafari!",
+      description: "Your Urban StoryTelling Guide to explore the world around you ",
+      image: "../assets/imgs/bysafari_larry_intro.png",
+    },
+    {
+      title: "We Need your GPS HELP?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "../assets/imgs/bysafari_larry_intro.png",
+    },
+    {
+      title: "Explore and Be Safe?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "../assets/imgs/bysafari_larry_intro.png",
+    }
+  ];
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -34,3 +52,4 @@ export class IntroPage {
   }
 
 }
+
