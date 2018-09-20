0250c8b9962f3636bff7ab635786dc314887e5e3
Jessica Thomsen <>
2018-09-20 11:03:38 +0200

    Introduction Slider structure

--- MyPrototype/src/app/app.module.ts
+++ MyPrototype/src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
+
+
+
+import { IntroPage } from '../pages/intro/intro';
 import { HomePage } from '../pages/home/home';
+
 import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { IntroPage } from '../pages/intro/intro';
 import { BirdPage } from '../pages/bird/bird';
 import { BeePage } from '../pages/bee/bee';
 import { MonsterPage } from '../pages/monster/monster';
@@ -19,9 +22,9 @@ import { WaterPage } from '../pages/water/water';
 @NgModule({
   declarations: [
     MyApp,
+    IntroPage,
     HomePage,
     ListPage,
-    IntroPage,
     BirdPage,
     BeePage,
     MonsterPage,
@@ -35,9 +38,9 @@ import { WaterPage } from '../pages/water/water';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    IntroPage,
     HomePage,
     ListPage,
-    IntroPage,
     BirdPage,
     BeePage,
     MonsterPage,
--- MyPrototype/src/pages/intro/intro.ts
+++ MyPrototype/src/pages/intro/intro.ts
@@ -1,6 +1,7 @@
+import { HomePage } from '../home/home';
+
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
 
 /**
  * Generated class for the IntroPage page.
