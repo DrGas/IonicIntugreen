8af589c4161f56f20db1716fcdbd8653c725ab30
DrGas <magdalena.drgas@gmail.com>
2018-09-21 10:59:35 +0200

    video-test

--- MyPrototype/src/pages/water/water.ts
+++ MyPrototype/src/pages/water/water.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-
+import { VideoPlayer, VideoOptions } from '@ionic-native/video-player';
 
 /**
  * Generated class for the WaterPage page.
@@ -17,15 +17,30 @@ import { HomePage } from '../home/home';
 })
 export class WaterPage {
 	homePage = HomePage;
+	videoOptions: VideoOptions;
+	videoUrl: string;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private videoPlayer: VideoPlayer, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   goHome(){
   	this.navCtrl.setRoot(this.homePage);
   }
 
+  async playVideo(){
+  	try {
+  		this.videoOptions = {
+  			volume: 0.7,
+  		}
+  	this.videoUrl = "https://vimeo.com/206216572"	
+  	this.videoPlayer.play(this.videoUrl, this.videoOptions)
+
+  	}catch(e){
+  		console.error(e);
+  	}
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WaterPage');
