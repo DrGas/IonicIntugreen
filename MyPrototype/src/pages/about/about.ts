42b5d018e93e405e9062bc1d8ba0eb70164b5fe2
DrGas <magdalena.drgas@gmail.com>
2018-09-24 13:45:32 +0200

    About navigation 03

--- MyPrototype/src/pages/about/about.ts
+++ MyPrototype/src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the AboutPage page.
@@ -14,10 +15,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'about.html',
 })
 export class AboutPage {
+		homePage = HomePage;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
+
+  goHome(){
+  	this.navCtrl.setRoot(this.homePage);
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
   }
